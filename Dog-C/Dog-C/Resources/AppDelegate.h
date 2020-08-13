6db9f99657921244b26764777f3719675a6b441a
Ian Becker <ian-becker@lambdastudents.com>
2020-08-13 08:23:06 -0700

    Project created

---
+++ Dog-C/Dog-C/Resources/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Dog-C
+//
+//  Created by Ian Becker on 8/13/20.
+//  Copyright © 2020 Ian Becker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Dog-C/Dog-C/Resources/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  Dog-C
+//
+//  Created by Ian Becker on 8/13/20.
+//  Copyright © 2020 Ian Becker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
